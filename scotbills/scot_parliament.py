52d8412d5ec529a2ba64a6ba8ef698a1a8cba3a3
king-millez <Millez.Dev@gmail.com>
2020-10-28 11:53:02 +1100

    Add session 1 - 4 bills

--- scotbills/scot_parliament.py
+++ scotbills/scot_parliament.py
@@ -1,6 +1,9 @@
-from requests import post
+from requests import post, get
 from bs4 import BeautifulSoup
+import json
 
+old_base = 'https://www.parliament.scot'
+old_site_base = 'https://www.parliament.scot/parliamentarybusiness/Bills/29732.aspx'
 beta_url = 'https://beta.parliament.scot/api/sitecore/BillListing/SearchBills' # POST URL
 beta_strip = 'https://beta.parliament.scot'
 headers = {'User-Agent': 'Hello from https://github.com/king-millez/Scot-Bills !'}
@@ -15,8 +18,8 @@ class All_Bills(object):
 
     def __init__(self):
         try:
+            self._scrape_old_site()
             self._scrape_beta_site()
-            #self._scrape_old_site()
         except Exception as e:
             raise Exception('Don\'t worry, the errors don\'t have an accent.\n' + e)
         
@@ -33,7 +36,7 @@ class All_Bills(object):
             table = soup.find('div', {'id': 'paneltab2'})
             rows = table.find_all('div', {'class': 'bill-panel u--shadow'})
             for row in rows:
-                _bill_title = row.find('a').text
+                _bill_title = row.find('a').text.replace('\u2019', '\'')
                 _bill_url = beta_strip + row.find('a')['href']
                 _bill_type = row.find('p').text
                 _bill_status = self.get_status(row.find('text', {'class': 'box__text'}).text)
@@ -57,11 +60,21 @@ class All_Bills(object):
         else:
             raise ValueError('Use a str for the bill status...\n' + str(_input))
 
+    def _scrape_old_site(self):
+        soup = BeautifulSoup(get(old_site_base, headers=headers).text, 'lxml')
+        div = soup.find('div', {'class': 'secondaryContent'})
+        for session in div.find_all('div', {'class': 'podHalf'})[1:]:
+            soup = BeautifulSoup(get(old_base + session.find('a')['href'], headers=headers).text, 'lxml')
+            row = soup.find('div', {'class': 'secondaryContent'})
+            table = row.find_all('div', {'class': 'podHalf'})
+            for row in table:
+                _bill_title = row.text.strip()
+                _bill_url = old_base + row.find('a')['href']
+                bill_dict = {URL: _bill_url, TITLE: _bill_title}
+                self._bills_data.append(bill_dict)
+
     @property
     def data(self):
         return(self._bills_data)
 
 all_bills = All_Bills().data
-
-for bill in all_bills:
-    print(bill)
